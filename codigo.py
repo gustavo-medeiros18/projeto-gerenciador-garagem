47f558b9f2001daa429b11ce609ae1049772ab19
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2026-03-15 19:50:17 -0300

    Cria modulo de dao para responsabilidade de persistencia

--- codigo.py
+++ codigo.py
@@ -9,28 +9,8 @@
 # .\.venv\Scripts\activate
 # python -m pip install tabulate
 
-import json
 from tabulate import tabulate
-
-def ler_carros_arquivo():
-    try:
-        with open("carros.json", "r") as arquivo_json:
-            lista_convertida = json.load(arquivo_json)
-            return lista_convertida
-    except FileNotFoundError:
-        print("Primeira execução. Arquivo vazio ou inexistente.")
-
-        lista_convertida = []
-        return lista_convertida
-    except json.decoder.JSONDecodeError:
-        print("Conteúdo do arquivo não pode ser convertido.")
-
-        lista_convertida = []
-        return lista_convertida
-
-def salvar_carros(lista_carros):
-    with open("carros.json", "w") as arquivo_json:
-        json.dump(lista_carros, arquivo_json, indent=2)
+from dao.carros_dao import ler_carros_arquivo, salvar_carros
 
 def encontrar_carro(placa, lista_carros):
     carro_encontrado = None
